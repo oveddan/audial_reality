ceaa0dd30da1b07a986cb8d640e35dce9388f38c
Ian McNally <imcnally@stridenyc.com>
2015-12-31 09:56:20 -0500

    bootstrap app in non-test environments. closes #3.

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { render } from 'react-dom'
 import styles from './app.css'
 
 export default class App extends Component {
@@ -8,4 +9,7 @@ export default class App extends Component {
   }
 
 }
+
+if (process.env.NODE_ENV !== 'test')
+  render(<App />, document.getElementById('main'))
 
