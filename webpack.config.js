e5f01bee6e8dafe1d2db951134e41bc3a711b552
Ian McNally <ianmcnally@gmail.com>
2016-03-04 10:31:52 -0500

    remove lodash lib dependency, and pull in assign for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-var,object-shorthand,prefer-template */
 
 var path = require('path')
-var extend = require('lodash/object/extend')
+var assign = require('lodash.assign')
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
@@ -54,11 +54,11 @@ var config = {
   ]
 }
 
-var buildConfig = extend({}, config, {
+var buildConfig = assign({}, config, {
   entry : [PATHS.SOURCE + '/app.jsx']
 })
 
-var devConfig = extend({}, buildConfig, {
+var devConfig = assign({}, buildConfig, {
   entry : [
     'webpack-hot-middleware/client'
   ].concat(buildConfig.entry),
