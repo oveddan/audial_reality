c71c8c9c853b65ccb492efbad8b6ff1985973519
Ian McNally <imcnally@stridenyc.com>
2016-07-01 14:47:27 -0400

    use render-shallow in tests

--- src/__tests__/app.spec.js
+++ src/__tests__/app.spec.js
@@ -1,10 +1,17 @@
 import App from 'src/app'
 import { expect } from 'chai'
+import renderShallow from 'render-shallow'
+import React from 'react'
 
 describe('App', () => {
+  let component
+
+  before(() => {
+    component = renderShallow(<App />).output
+  })
 
   it('exists', () => {
-    expect(App).to.be.ok
+    expect(component).to.be.ok
   })
 
 })
